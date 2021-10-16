ec023a17444334e7e9904d9729ece99d9a48b8af
Max <max@privatevoid.net>
2021-10-16 17:03:40 +0200

    tools/networks: init

--- tools/default.nix
+++ tools/default.nix
@@ -2,6 +2,7 @@ let toolsets = {
     meta = import ./meta.nix;
 
     identity = import ./identity.nix { inherit toolsets; };
+    networks = import ./networks.nix { inherit toolsets; };
   };
 in toolsets // {
   all = args: (builtins.mapAttrs (_: x: x args) toolsets) // { inherit (toolsets) meta; };
