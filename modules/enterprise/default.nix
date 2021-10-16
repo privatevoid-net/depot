a0fa47ddda6ca88ffd644f0a011aaf991d5a29cb
Max <max@privatevoid.net>
2021-10-16 14:27:30 +0200

    modules/enterprise: install CA certificate

--- modules/enterprise/default.nix
+++ modules/enterprise/default.nix
@@ -30,4 +30,5 @@ in {
   services.pcscd.enable = true;
   networking.domain = lib.mkDefault "${host.enterprise.subdomain or "services"}.${orgDomain}";
   networking.search = [ config.networking.domain "search.${orgDomain}" ];
+  security.pki.certificates = [ (builtins.readFile ../../data/ca.crt) ];
 }
