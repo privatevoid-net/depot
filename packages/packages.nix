d6ed143a42052f58ecc9bb68c385709867aed6e4
Max <max@privatevoid.net>
2022-03-13 00:23:41 +0100

    packages: switch to dream2nix lib2

--- packages/packages.nix
+++ packages/packages.nix
@@ -1,29 +1,25 @@
 { pkgs, inputs, ... }:
 let
   inherit (pkgs) system;
-  dream2nix = inputs.dream2nix.lib.init {
+  dream2nix = inputs.dream2nix.lib2.init {
     systems = [ system ];
-    config.overridesDirs = [ ./dream2nix-overrides ];
+    config = {
+      projectRoot = ./.;
+      overridesDirs = [ ./dream2nix-overrides ];
+    };
   };
   poetry2nix = pkgs.poetry2nix.overrideScope' (final: prev: {
     defaultPoetryOverrides = prev.defaultPoetryOverrides.extend (import ./poetry2nix-overrides);
   });
 in
 {
-  ghost = (dream2nix.riseAndShine {
-    source = ./servers/ghost/dream-lock.json;
-    sourceOverrides = oldSources: let
-      version = "4.32.3";
-    in {
-
-      # building ghost ourselves is a pain in the ass, so just use the zip
-      "ghost"."${version}" = pkgs.fetchzip {
-        url = "https://github.com/TryGhost/Ghost/releases/download/v${version}/Ghost-${version}.zip";
-        sha256 = "sha256-XneO6es3eeJz4v1JnWtUfm27zwUW2Wy3hTIHUF7UrFc=";
-        stripRoot = false;
-      };
+  ghost = (let version = "4.32.3"; in dream2nix.makeFlakeOutputs {
+    source = pkgs.fetchzip {
+      url = "https://github.com/TryGhost/Ghost/releases/download/v${version}/Ghost-${version}.zip";
+      sha256 = "sha256-XneO6es3eeJz4v1JnWtUfm27zwUW2Wy3hTIHUF7UrFc=";
+      stripRoot = false;
     };
-  }).defaultPackage.${system};
+  }).packages.${system}.ghost;
 
   hyprspace = pkgs.callPackage ./networking/hyprspace { iproute2mac = null; };
 
