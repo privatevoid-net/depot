ce7c30c832b5fd6ca50f8c6f598c00babf7d70b8
Max <max@privatevoid.net>
2022-05-27 17:20:36 +0200

    packages/sssd: remove gcc reference

--- packages/patched-derivations.nix
+++ packages/patched-derivations.nix
@@ -12,7 +12,12 @@ super: rec {
 
   lain-ipfs = patch-rename (super.ipfs_latest or super.ipfs) "lain-ipfs" "patches/base/ipfs";
 
-  sssd = super.sssd.override { withSudo = true; };
+  sssd = (super.sssd.override { withSudo = true; }).overrideAttrs (old: {
+    postFixup = (old.postFixup or "") + ''
+      ${super.removeReferencesTo}/bin/remove-references-to -t ${super.stdenv.cc.cc} $out/modules/ldb/memberof.so
+    '';
+    disallowedReferences = [ super.stdenv.cc.cc ];
+  });
 
   jre17_standard = super.jre_minimal.override {
     jdk = super.jdk17_headless;
