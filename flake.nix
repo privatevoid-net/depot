3b1b18781846ba207c27e8d5e0a4270f075688e4
Max <max@privatevoid.net>
2022-05-29 18:11:27 +0200

    meta: nixpkgs 21.11 -> 22.05

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Private Void system configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11-small";
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05-small";
 
     nix-super.url = "git+https://git.privatevoid.net/max/nix-super-fork";
     nix-super.inputs.nixpkgs.follows = "nixpkgs";
