bd5f155e23e0c49b6191e510e4a2a3837de0d810
Max <max@privatevoid.net>
2022-05-31 11:40:00 +0200

    checks/keycloak: init

--- packages/tests/default.nix
+++ packages/tests/default.nix
@@ -3,4 +3,21 @@ let
   inherit (pkgs) nixosTest;
 in
 {
+  keycloak = nixosTest {
+    name = "keycloak";
+    nodes.machine.services.keycloak = {
+      enable = true;
+      package = pkgs.keycloak.override { jre = inputs.self.packages.${system}.jre17_standard; };
+      database.passwordFile = builtins.toFile "keycloak-test-password" "kcnixostest1234";
+      settings = {
+        proxy = "edge";
+        hostname = "keycloak.local";
+      };
+    };
+    testScript = ''
+      machine.wait_for_unit("keycloak.service")
+      machine.wait_for_open_port("80")
+      machine.succeed("curl --fail http://127.0.0.1:80")
+    '';
+  };
 }
