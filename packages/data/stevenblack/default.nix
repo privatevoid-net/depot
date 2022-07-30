8e6543c662c512811eae3973c05b3e7ffc04fb2f
Max <max@privatevoid.net>
2022-07-30 19:49:22 +0200

    packages/stevenblack-hosts: rename pin to stevenblack-hosts

--- packages/data/stevenblack/default.nix
+++ packages/data/stevenblack/default.nix
@@ -1,12 +1,12 @@
 { stdenvNoCC, npins, pins }:
 
 let
-  src = npins.mkSource pins.stevenblack;
+  src = npins.mkSource pins.stevenblack-hosts;
 in
 
 stdenvNoCC.mkDerivation {
   pname = "stevenblack-hosts";
-  inherit (pins.stevenblack) version;
+  inherit (pins.stevenblack-hosts) version;
   buildCommand = ''
     cp ${src}/hosts $out
   '';
