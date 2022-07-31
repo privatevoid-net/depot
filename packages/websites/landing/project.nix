cff89b6864874634a27bf0f2bc107dcb11ce5799
Max <max@privatevoid.net>
2022-07-31 16:40:33 +0200

    packages/landing: base configuration

--- packages/websites/landing/project.nix
+++ packages/websites/landing/project.nix
@@ -1,9 +1,26 @@
-{ pkgs, ... }:
+{ lib, pkgs, self', ... }:
+let
+  theme = self'.packages.gohugo-theme-ananke;
+  themeName = "ananke";
 
+  themesDir = "${theme}/share/hugo/themes";
+
+  configFile = pkgs.writeText "hugo-config.json" (builtins.toJSON {
+    title = "Private Void | Zero-maintenance perfection";
+    baseURL = "https://www.privatevoid.net/";
+    languageCode = "en-us";
+    theme = themeName;
+    inherit themesDir;
+  });
+  hugoArgs = [
+    "--config" configFile
+  ];
+in
 {
   projectShells.landing = {
-    tools = with pkgs; [
-      hugo
-    ];
+    commands.hugo = {
+      help = pkgs.hugo.meta.description;
+      command = "exec ${pkgs.hugo}/bin/hugo ${lib.concatStringsSep " " hugoArgs} \"$@\"";
+    };
   };
 }
