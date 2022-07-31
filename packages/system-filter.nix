e72161028b73188e030df1bba1b6ce82e5396b79
Max <max@privatevoid.net>
2022-07-31 22:43:20 +0200

    packages: add more per-architecture filters

--- packages/system-filter.nix
+++ packages/system-filter.nix
@@ -1,6 +1,13 @@
 {
   packages = {
+    grafana = [ "x86_64-linux" ];
+    hci = [ "x86_64-linux" ];
+    hydra = [ "x86_64-linux" ];
     keycloak = [ "x86_64-linux" ];
+    reflex-cache = [ "aarch64-linux" ];
+    searxng = [ "x86_64-linux" ];
+    sips = [ "x86_64-linux" ];
+    tempo = [ "x86_64-linux" ];
     uptime-kuma = [ "x86_64-linux" ];
   };
   checks = {
