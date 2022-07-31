453bca08b3775dd633ea42cde06da92ba4922a29
Max <max@privatevoid.net>
2022-07-31 22:53:59 +0200

    VEGAS/mc-num: use standard JRE

--- hosts/VEGAS/services/minecraft/num.nix
+++ hosts/VEGAS/services/minecraft/num.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, inputs, pkgs, ... }:
 {
   links.mc-num = {};
   services.modded-minecraft-servers.instances.num = {
@@ -6,7 +6,7 @@
     rsyncSSHKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5C7mC5S2gM0K6x0L/jNwAeQYbFSzs16Q73lONUlIkL" # max@TITAN
     ];
-    jvmPackage = pkgs.jre;
+    jvmPackage = inputs.self.packages.${pkgs.system}.jre17_standard;
     jvmInitialAllocation = "2G";
     jvmMaxAllocation = "8G";
     serverConfig = {
