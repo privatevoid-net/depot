b396db4f56de220133094fcd19f3222e0fb79afb
Max <max@privatevoid.net>
2022-07-31 22:49:19 +0200

    packages: unfilter reflex-cache

--- packages/system-filter.nix
+++ packages/system-filter.nix
@@ -4,7 +4,6 @@
     hci = [ "x86_64-linux" ];
     hydra = [ "x86_64-linux" ];
     keycloak = [ "x86_64-linux" ];
-    reflex-cache = [ "aarch64-linux" ];
     searxng = [ "x86_64-linux" ];
     sips = [ "x86_64-linux" ];
     tempo = [ "x86_64-linux" ];
