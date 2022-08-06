e5c1bc297b12544510b9e219f498f480915875ce
Max <max@privatevoid.net>
2022-08-06 21:32:34 +0200

    packages/stop-using-nix-env: name -> pname

--- packages/websites/stop-using-nix-env/project.nix
+++ packages/websites/stop-using-nix-env/project.nix
@@ -3,16 +3,16 @@
 {
   packages.stop-using-nix-env = let
     site = with pkgs; stdenvNoCC.mkDerivation rec {
-      name = "stop-using-nix-env";
+      pname = "stop-using-nix-env";
       version = "1.1.1";
       src = ./src;
       buildCommand = ''
-        install -Dm644 $src/* -t $out/share/www/${name}
-        substituteInPlace $out/share/www/${name}/index.html \
+        install -Dm644 $src/* -t $out/share/www/${pname}
+        substituteInPlace $out/share/www/${pname}/index.html \
           --replace '<!-- VERSION -->' 'Version ${version} |'
       '';
       passthru = {
-        webroot = "${site}/share/www/${name}";
+        webroot = "${site}/share/www/${pname}";
       };
     };
   in site;
