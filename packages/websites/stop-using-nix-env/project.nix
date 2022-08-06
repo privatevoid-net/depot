9976784890584ac65c909baf20616dc6dd836d68
Max <max@privatevoid.net>
2022-08-06 21:29:54 +0200

    packages/stop-using-nix-env: add footer with link to repo

--- packages/websites/stop-using-nix-env/project.nix
+++ packages/websites/stop-using-nix-env/project.nix
@@ -4,10 +4,12 @@
   packages.stop-using-nix-env = let
     site = with pkgs; stdenvNoCC.mkDerivation rec {
       name = "stop-using-nix-env";
-      version = "1.1.0";
+      version = "1.1.1";
       src = ./src;
       buildCommand = ''
         install -Dm644 $src/* -t $out/share/www/${name}
+        substituteInPlace $out/share/www/${name}/index.html \
+          --replace '<!-- VERSION -->' 'Version ${version} |'
       '';
       passthru = {
         webroot = "${site}/share/www/${name}";
