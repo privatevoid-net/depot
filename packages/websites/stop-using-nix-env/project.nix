f2c859b0cfd86474f2960d16ff2c91eba853254e
Max <max@privatevoid.net>
2022-08-06 16:22:26 +0200

    packages/stop-using-nix-env: apply suggestion by NobbZ
    Co-authored-by: Norbert Melzer <timmelzer@gmail.com>

--- packages/websites/stop-using-nix-env/project.nix
+++ packages/websites/stop-using-nix-env/project.nix
@@ -4,7 +4,7 @@
   packages.stop-using-nix-env = let
     site = with pkgs; stdenvNoCC.mkDerivation rec {
       name = "stop-using-nix-env";
-      version = "1.0.0";
+      version = "1.0.1";
       src = ./src;
       buildCommand = ''
         install -Dm644 $src/* -t $out/share/www/${name}
