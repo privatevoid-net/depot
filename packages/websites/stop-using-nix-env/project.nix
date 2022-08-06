8e05127b05c8a4241ef7965def1ee33a191122e4
Max <max@privatevoid.net>
2022-08-07 00:41:49 +0200

    packages/stop-using-nix-env: fix a typo

--- packages/websites/stop-using-nix-env/project.nix
+++ packages/websites/stop-using-nix-env/project.nix
@@ -4,7 +4,7 @@
   packages.stop-using-nix-env = let
     site = with pkgs; stdenvNoCC.mkDerivation rec {
       pname = "stop-using-nix-env";
-      version = "1.1.1";
+      version = "1.1.2";
       src = ./src;
       buildCommand = ''
         install -Dm644 $src/* -t $out/share/www/${pname}
