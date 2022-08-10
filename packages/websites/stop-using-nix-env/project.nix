876a2eba9805a76143347b0dfa5860cfd6bb311e
Max <max@privatevoid.net>
2022-08-10 20:44:58 +0200

    packages/stop-using-nix-env: Mention implicit package pinning
    Co-authored-by: Fernando Ayats <ayatsfer@gmail.com>

--- packages/websites/stop-using-nix-env/project.nix
+++ packages/websites/stop-using-nix-env/project.nix
@@ -4,7 +4,7 @@
   packages.stop-using-nix-env = let
     site = with pkgs; stdenvNoCC.mkDerivation rec {
       pname = "stop-using-nix-env";
-      version = "1.1.2";
+      version = "1.2.0";
       src = ./src;
       buildCommand = ''
         install -Dm644 $src/* -t $out/share/www/${pname}
