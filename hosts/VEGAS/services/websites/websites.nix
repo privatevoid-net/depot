dac9c2fb3c9f2e044ec1e1b308facc3d7f867d8c
Max <max@privatevoid.net>
2022-08-28 20:18:12 +0200

    VEGAS/websites: add back custom Soda error pages

--- hosts/VEGAS/services/websites/websites.nix
+++ hosts/VEGAS/services/websites/websites.nix
@@ -1,11 +1,15 @@
 { packages, tools }:
 with tools.vhosts;
-let inherit (tools) domain; in
 {
   # websites
   ktp    = static "/srv/storage/www/soda/ktp";
   legacy = static "/srv/storage/www/legacy";
-  soda   = static "/srv/storage/www/soda"; # TODO: add back custom error pages, wttr.in cache
+  soda   = static "/srv/storage/www/soda" // {
+    extraConfig = ''
+      error_page 404 /.nginx-private/404.html;
+      error_page 500 502 503 504 /.nginx-private/50x.html;
+    '';
+  };
 
   # content delivery
   autoconfig = static "/srv/storage/www/autoconfig";
