e2a9f1a1ab9d15b41e08379a917ae9eb1222811b
Max <max@privatevoid.net>
2022-08-28 16:33:25 +0200

    checks/patroni: init

--- packages/checks/default.nix
+++ packages/checks/default.nix
@@ -1,8 +1,12 @@
-{ filters, inputs', pkgs, self', ... }:
+{ filters, inputs', pkgs, self, self', ... }:
 {
   checks = filters.doFilter filters.checks {
     keycloak = pkgs.callPackage ./keycloak-custom-jre.nix {
       jre = self'.packages.jre17_standard;
+    };
+
+    patroni = pkgs.callPackage ./patroni.nix {
+      patroniModule = self.nixosModules.patroni;
     };
   };
 }
