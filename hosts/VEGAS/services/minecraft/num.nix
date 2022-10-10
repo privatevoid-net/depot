c0c79c446ce8183e03711565dce996d1a318520b
redshifttt <num@privatevoid.net>
2022-10-10 21:17:40 +0100

    VEGAS/minecraft: disable num server

--- hosts/VEGAS/services/minecraft/num.nix
+++ hosts/VEGAS/services/minecraft/num.nix
@@ -2,7 +2,7 @@
 {
   links.mc-num = {};
   services.modded-minecraft-servers.instances.num = {
-    enable = true;
+    enable = false;
     rsyncSSHKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5C7mC5S2gM0K6x0L/jNwAeQYbFSzs16Q73lONUlIkL" # max@TITAN
     ];
