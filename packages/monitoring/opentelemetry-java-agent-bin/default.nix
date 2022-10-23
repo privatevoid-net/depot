606462dacb103c3c696cfdb92c85eeb9f5e0894d
Max <max@privatevoid.net>
2022-10-23 16:54:18 +0200

    packages/opentelemetry-java-agent-bin: 1.15.0 -> 1.19.1

--- packages/monitoring/opentelemetry-java-agent-bin/default.nix
+++ packages/monitoring/opentelemetry-java-agent-bin/default.nix
@@ -2,7 +2,7 @@
 
 fetchurl rec {
   name = "opentelemetry-java-agent-${meta.version}.jar";
-  meta.version = "1.15.0";
+  meta.version = "1.19.1";
   url = "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v${meta.version}/opentelemetry-javaagent.jar";
-  sha256 = "sha256-FoHax7pS3ohZ70TCKVkGsAONDmr4/FFjY5SSoCfySy0=";
+  sha256 = "sha256-f1kc0eqBrK+QmlRaZRiJq5OAKa2wrtTyLeBN8uK6698=";
 }
