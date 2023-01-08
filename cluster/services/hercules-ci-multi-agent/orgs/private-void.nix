90abb5792e1064f7160a7a3c3f4df84c4ceac278
Max <max@privatevoid.net>
2023-01-08 20:34:55 +0100

    cluster/services/hercules-ci-multi-agent: use our patched hercules-ci-agent

--- cluster/services/hercules-ci-multi-agent/orgs/private-void.nix
+++ cluster/services/hercules-ci-multi-agent/orgs/private-void.nix
@@ -1,7 +1,8 @@
-{ config, inputs, pkgs, ... }:
+{ config, lib, inputs, pkgs, ... }:
 
 {
   services.hercules-ci-agents.private-void = {
+    package = lib.mkForce inputs.self.packages.${pkgs.system}.hercules-ci-agent;
     settings = {
       clusterJoinTokenPath = config.age.secrets.hci-token-private-void.path;
       binaryCachesPath = config.age.secrets.hci-cache-config-private-void.path;
