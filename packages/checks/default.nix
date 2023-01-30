7fda7ca695c56a80ff35ddd9747e0a772f56195f
Max <max@privatevoid.net>
2023-01-31 00:00:53 +0100

    checks/searxng: init

--- packages/checks/default.nix
+++ packages/checks/default.nix
@@ -10,6 +10,9 @@
       patroni = pkgs.callPackage ./patroni.nix {
         patroniModule = self.nixosModules.patroni;
       };
+      searxng = pkgs.callPackage ./searxng.nix {
+        inherit (self'.packages) searxng;
+      };
       tempo = pkgs.callPackage ./tempo.nix {
         inherit (self'.packages) tempo;
       };
---
+++ packages/checks/searxng.nix
@@ -0,0 +1,19 @@
+{ nixosTest, searxng, writeText }:
+
+nixosTest {
+  name = "searxng";
+  nodes.machine =  {
+    services.searx = {
+      enable = true;
+      runInUwsgi = true;
+      package = searxng;
+      settings.server.secret_key = "NixOSTestKey";
+      uwsgiConfig.http = "0.0.0.0:8080";
+    };
+  };
+  testScript = ''
+    machine.wait_for_unit("uwsgi.service")
+    machine.wait_for_open_port(8080)
+    machine.succeed("curl --fail http://127.0.0.1:8080/")
+  '';
+}
