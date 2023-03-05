63b995792696570dd1c00b5f90670feffe593afd
Max <max@privatevoid.net>
2023-03-06 00:36:17 +0100

    cluster/services/websites: register with consul

--- cluster/services/websites/host.nix
+++ cluster/services/websites/host.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, pkgs, tools, ... }:
+{ config, inputs, lib, pkgs, tools, ... }:
 
 let
   importWebsites = expr: import expr {
@@ -20,4 +20,17 @@ let
 in {
   services.nginx.virtualHosts = websites;
   security.acme.certs = lib.mapAttrs' acmeUseDNS (lib.filterAttrs isACME websites);
+
+  consul.services.nginx = {
+    mode = "external";
+    definition.service = {
+      name = "static-lb";
+      address = lib.toLower "${config.networking.hostName}.${config.networking.domain}";
+      port = 443;
+      checks = lib.singleton {
+        interval = "60s";
+        tcp = "127.0.0.1:80";
+      };
+    };
+  };
 }
