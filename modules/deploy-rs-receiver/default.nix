39050c9161db44a60d3d81929bcb2ffdcf1ba880
Max <max@privatevoid.net>
2023-03-22 18:14:40 +0100

    modules/deploy-rs-receiver: add another key

--- modules/deploy-rs-receiver/default.nix
+++ modules/deploy-rs-receiver/default.nix
@@ -15,6 +15,7 @@
     uid = 1999;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmdWfmAs/0rno8zJlhBFMY2SumnHbTNdZUXJqxgd9ON max@jericho"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5C7mC5S2gM0K6x0L/jNwAeQYbFSzs16Q73lONUlIkL max@TITAN"
     ];
   };
 }
