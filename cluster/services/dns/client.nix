ae07af5a803dd8e8a55eddd515dfe67dc42ea9ee
Max <max@privatevoid.net>
2023-03-22 23:23:14 +0100

    cluster/services/dns: client: use more DNS servers

--- cluster/services/dns/client.nix
+++ cluster/services/dns/client.nix
@@ -1,5 +1,11 @@
-{ cluster, ... }:
+{ cluster, lib, ... }:
+
+let
+  recursors = lib.pipe (cluster.config.services.dns.nodes.coredns) [
+    (map (node: cluster.config.hostLinks.${node}.dnsResolverBackend.ipv4))
+  ];
+in
 
 {
-  networking.nameservers = [ cluster.config.links.dnsResolver.ipv4 ];
+  networking.nameservers = [ cluster.config.links.dnsResolver.ipv4 ] ++ recursors;
 }
