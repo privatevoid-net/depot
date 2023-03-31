e8d350b9a31805a21793c9b6ef5d0058bc81ca55
Max <max@privatevoid.net>
2023-03-31 16:03:03 +0200

    cluster/services/hercules-ci-multi-agent: remove custom agent package

--- cluster/services/hercules-ci-multi-agent/orgs/max.nix
+++ cluster/services/hercules-ci-multi-agent/orgs/max.nix
@@ -2,7 +2,6 @@
 
 {
   services.hercules-ci-agents.max = {
-    package = lib.mkForce depot.packages.hercules-ci-agent;
     settings = {
       clusterJoinTokenPath = config.age.secrets.hci-token-max.path;
       binaryCachesPath = config.age.secrets.hci-cache-config-max.path;
--- cluster/services/hercules-ci-multi-agent/orgs/nixpak.nix
+++ cluster/services/hercules-ci-multi-agent/orgs/nixpak.nix
@@ -2,7 +2,6 @@
 
 {
   services.hercules-ci-agents.nixpak = {
-    package = lib.mkForce depot.packages.hercules-ci-agent;
     settings = {
       clusterJoinTokenPath = config.age.secrets.hci-token-nixpak.path;
       binaryCachesPath = config.age.secrets.hci-cache-config-nixpak.path;
