6624e2b7c7ea20700a3c339764e8eb40aa86fc16
Max <max@privatevoid.net>
2023-05-27 14:18:02 +0200

    cluster/services/monitoring: refactor

--- cluster/services/monitoring/client.nix
+++ cluster/services/monitoring/client.nix
@@ -1,6 +1,6 @@
 { cluster, lib, ... }:
 let
-  myNode = cluster.config.vars.mesh.${cluster.config.vars.hostName};
+  inherit (lib) singleton;
 
   relabel = from: to: {
     source_labels = [ from ];
@@ -12,7 +12,7 @@ in {
   services.grafana-agent = {
     enable = true;
     settings = {
-      metrics.global.remote_write = lib.singleton {
+      metrics.global.remote_write = singleton {
         url = "${cluster.config.links.prometheus-ingest.url}/api/v1/write";
       };
       integrations.node_exporter = {
@@ -22,30 +22,28 @@ in {
           "systemd"
         ];
       };
-      logs.configs = lib.singleton {
+      logs.configs = singleton {
         name = "logging";
         positions.filename = "\${STATE_DIRECTORY:/tmp}/logging-positions.yaml";
-        clients = [
-          { url = "${cluster.config.links.loki-ingest.url}/loki/api/v1/push"; }
-        ];
-        scrape_configs = [
-          {
-            job_name = "journal";
-            journal = {
-              max_age = "12h";
-              labels.host = cluster.config.vars.hostName;
-            };
-              relabel_configs = [
-                (relabel "__journal__systemd_unit" "systemd_unit")
-                (relabel "__journal__hostname" "machine_name")
-                (relabel "__journal__exe" "executable")
-                (relabel "__journal__comm" "command")
-                (relabel "__journal__boot_id" "systemd_boot_id")
-                (relabel "__journal__systemd_cgroup" "systemd_cgroup")
-                (relabel "__journal_syslog_identifier" "syslog_identifier")
-              ];
-          }
-        ];
+        clients = singleton {
+          url = "${cluster.config.links.loki-ingest.url}/loki/api/v1/push";
+        };
+        scrape_configs = singleton {
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels.host = cluster.config.vars.hostName;
+          };
+          relabel_configs = [
+            (relabel "__journal__systemd_unit" "systemd_unit")
+            (relabel "__journal__hostname" "machine_name")
+            (relabel "__journal__exe" "executable")
+            (relabel "__journal__comm" "command")
+            (relabel "__journal__boot_id" "systemd_boot_id")
+            (relabel "__journal__systemd_cgroup" "systemd_cgroup")
+            (relabel "__journal_syslog_identifier" "syslog_identifier")
+          ];
+        };
       };
     };
   };
