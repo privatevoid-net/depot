f41265ed000dfd4902815e1685c98b7640fc7f17
Max <max@privatevoid.net>
2023-06-02 17:38:12 +0200

    cluster/services/ipfs: switch to cluster otlp trace endpoint

--- cluster/services/ipfs/monitoring.nix
+++ cluster/services/ipfs/monitoring.nix
@@ -1,11 +1,11 @@
-{ config, ... }:
+{ cluster, ... }:
 
 {
   systemd.services.ipfs = {
     environment = {
       OTEL_TRACES_EXPORTER = "otlp";
       OTEL_EXPORTER_OTLP_PROTOCOL = "grpc";
-      OTEL_EXPORTER_OTLP_ENDPOINT = config.links.tempo-otlp-grpc.url;
+      OTEL_EXPORTER_OTLP_ENDPOINT = cluster.config.links.tempo-otlp-grpc.url;
       OTEL_TRACES_SAMPLER = "parentbased_traceidratio";
       OTEL_TRACES_SAMPLER_ARG = "0.50";
     };
