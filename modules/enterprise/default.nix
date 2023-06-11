e34287cd05fb017a24f5da700c4b06bcf3458ca2
Max <max@privatevoid.net>
2023-06-11 17:30:03 +0200

    modules/enterprise: remove kerberos config

--- modules/enterprise/default.nix
+++ modules/enterprise/default.nix
@@ -1,31 +1,8 @@
 { config, depot, lib, tools, ... }:
 let
   orgDomain = tools.meta.domain;
-  orgRealm = lib.toUpper orgDomain;
   host = depot.reflection;
 in {
-  krb5 = {
-    enable = true;
-    domain_realm = {
-      ${orgDomain} = orgRealm;
-      ".${orgDomain}" = orgRealm;
-    };
-    libdefaults = {
-      default_realm = orgRealm;
-      dns_lookup_kdc = true;
-      rdns = false;
-      forwardable = true;
-      default_ccache_name = "KEYRING:persistent:%{uid}";
-    };
-    realms = {
-      "${orgRealm}" = rec {
-        inherit (tools.identity.kerberos) kdc;
-        admin_server = kdc;
-        kpasswd_server = kdc;
-        default_domain = orgDomain;
-      };
-    };
-  };
   networking.domain = lib.mkDefault "${host.enterprise.subdomain or "services"}.${orgDomain}";
   networking.search = [ config.networking.domain "search.${orgDomain}" ];
   security.pki.certificates = [ (builtins.readFile ../../data/ca.crt) ];
