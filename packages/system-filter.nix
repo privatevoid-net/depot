fcc807c7c803c525925ce80dc3110b7340ab8786
Max <max@privatevoid.net>
2023-07-09 16:18:06 +0200

    packages/jellyfin: only build on x86_64-linux

--- packages/system-filter.nix
+++ packages/system-filter.nix
@@ -4,6 +4,7 @@
     dvc = [ "x86_64-linux" ];
     hci = [ "x86_64-linux" ];
     hydra = [ "x86_64-linux" ];
+    jellyfin = [ "x86_64-linux" ];
     keycloak = [ "x86_64-linux" ];
     powerdns-admin = [ "x86_64-linux" ];
     prometheus-jitsi-exporter = [ "aarch64-linux" ];
