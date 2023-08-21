821a2ce92924dbf1229371af23013d2a38732668
Max <max@privatevoid.net>
2023-08-21 17:40:18 +0200

    packages/hyprspace: 0.6.3 -> 0.6.4
    - Go 1.19 -> 1.20
    - bump dependencies
    - switch to quic-v1 addresses

--- packages/networking/hyprspace/project.nix
+++ packages/networking/hyprspace/project.nix
@@ -4,13 +4,13 @@
   perSystem = { pkgs, ... }: {
     projectShells.hyprspace = {
       tools = [
-        pkgs.go_1_19
+        pkgs.go_1_20
       ];
       env.GOPATH.eval = "$REPO_DATA_DIR/go";
     };
-    packages.hyprspace = with pkgs; buildGo119Module {
+    packages.hyprspace = with pkgs; buildGo120Module {
       pname = "hyprspace";
-      version = "0.6.3";
+      version = "0.6.4";
 
       src = with inputs.nix-filter.lib; let
         dirs = map inDirectory;
@@ -29,7 +29,7 @@
         ]);
       };
 
-      vendorSha256 = "sha256-NMttBOpSMwUxhMNrLX6fn0u5Xc2FbY3xyE7FMOuev7c=";
+      vendorSha256 = "sha256-qdwJk8Cvwa7J2o+JQccTF6cnoiEGiJG0Nji7myvYmRQ=";
 
       meta = with lib; {
         description = "A Lightweight VPN Built on top of Libp2p for Truly Distributed Networks.";
