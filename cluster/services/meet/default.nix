163f111a819e81f95808240c400d41e3fd31c7ac
Max <max@privatevoid.net>
2023-09-04 16:39:54 +0200

    cluster/services/meet: remove blackbox check

--- cluster/services/meet/default.nix
+++ cluster/services/meet/default.nix
@@ -1,13 +1,6 @@
-{ depot, ... }:
-
 {
   services.meet = {
     nodes.host = [ "prophet" ];
     nixos.host = ./host.nix;
-  };
-
-  monitoring.blackbox.targets.jitsi-videobridge = {
-    address = "meet.${depot.lib.meta.domain}:7777";
-    module = "tcpConnect";
   };
 }
