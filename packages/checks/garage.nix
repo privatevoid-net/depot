123c5adef6e5d9ab708c76b5facf3fcac63db890
Max <max@privatevoid.net>
2023-09-05 23:17:09 +0200

    checks/garage: test declarative keys and buckets

--- packages/checks/garage.nix
+++ packages/checks/garage.nix
@@ -50,6 +50,19 @@ testers.runNixOSTest {
                 garage2 = { zone = "dc1"; capacity = 1000; };
                 garage3 = { zone = "dc1"; capacity = 1000; };
               };
+              keys.testKey.allow.createBucket = true;
+              buckets = {
+                bucket1 = {
+                  allow.testKey = [ "read" "write" ];
+                  quotas = {
+                    maxObjects = 300;
+                    maxSize = 400 * 1024 * 1024;
+                  };
+                };
+                bucket2 = {
+                  allow.testKey = [ "read" ];
+                };
+              };
             };
             system.ascensions.garage-layout.incantations = lib.mkForce (i: [
               (i.runGarage ''
@@ -81,9 +94,6 @@ testers.runNixOSTest {
 
     with subtest("should apply new layout with ascension"):
       for node in nodes:
-          node.systemctl("stop garage.service")
-
-      for node in nodes:
           node.succeed("/run/current-system/specialisation/modifiedLayout/bin/switch-to-configuration test")
 
       for node in nodes:
@@ -109,5 +119,20 @@ testers.runNixOSTest {
           node.wait_until_succeeds("garage layout show | grep -w 2000")
           assert "1" in node.succeed("garage layout show | grep -w 2000 | wc -l")
           assert "2" in node.succeed("garage layout show | grep -w 1000 | wc -l")
+
+    with subtest("should create specified buckets and keys"):
+      for node in nodes:
+          node.wait_until_succeeds('test "$(systemctl is-active garage-apply)" != activating')
+      garage1.succeed("garage key list | grep testKey")
+      garage1.succeed("garage bucket list | grep bucket1")
+      garage1.succeed("garage bucket list | grep bucket2")
+
+    with subtest("should delete unspecified buckets and keys"):
+      garage1.succeed("garage bucket create unwantedbucket")
+      garage1.succeed("garage key new --name unwantedkey")
+      garage1.succeed("systemctl restart garage-apply.service")
+
+      garage1.fail("garage key list | grep unwantedkey")
+      garage1.fail("garage bucket list | grep unwantedbucket")
   '';
 }
