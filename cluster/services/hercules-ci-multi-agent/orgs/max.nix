55d19314a91c46ac21cbf7d04e3532e94f90cd37
Max <max@privatevoid.net>
2023-10-26 02:14:20 +0200

    cluster/services/hercules-ci-multi-agent: use hercules-ci-agent package from flake

--- cluster/services/hercules-ci-multi-agent/orgs/max.nix
+++ cluster/services/hercules-ci-multi-agent/orgs/max.nix
@@ -3,6 +3,7 @@
 {
   services.hercules-ci-agents.max = {
     enable = true;
+    package = depot.inputs.hercules-ci-agent.packages.hercules-ci-agent;
     settings = {
       clusterJoinTokenPath = config.age.secrets.hci-token-max.path;
       binaryCachesPath = config.age.secrets.hci-cache-config-max.path;
--- cluster/services/hercules-ci-multi-agent/orgs/private-void.nix
+++ cluster/services/hercules-ci-multi-agent/orgs/private-void.nix
@@ -8,6 +8,7 @@
   };
   services.hercules-ci-agents.private-void = {
     enable = true;
+    package = depot.inputs.hercules-ci-agent.packages.hercules-ci-agent;
     settings = {
       clusterJoinTokenPath = config.age.secrets.hci-token-private-void.path;
       binaryCachesPath = config.age.secrets.hci-cache-config-private-void.path;
