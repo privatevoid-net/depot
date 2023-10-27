ed3d8fd7ab72e98b841c914b8c6229aa44785d74
Max <max@privatevoid.net>
2023-10-27 03:06:40 +0200

    packages/hyprspace: don't segfault when reading config fails

--- packages/networking/hyprspace/project.nix
+++ packages/networking/hyprspace/project.nix
@@ -10,7 +10,7 @@
     };
     packages.hyprspace = with pkgs; buildGo120Module rec {
       pname = "hyprspace";
-      version = "0.8.0";
+      version = "0.8.1";
 
       src = with inputs.nix-filter.lib; let
         dirs = map inDirectory;
