d6fe67c14e901f769f38487f6b09e62ea9da48a1
Max <max@privatevoid.net>
2023-11-04 23:36:19 +0100

    cluster/services/storage: use external s3 endpoint for remote storage

--- cluster/services/storage/external.nix
+++ cluster/services/storage/external.nix
@@ -9,8 +9,7 @@ in
     fileSystems.external = {
       mountpoint = "/srv/storage";
       authFile = ./secrets/external-storage-auth-${hostName}.age;
-      backend = "s3c://${cluster.config.hostLinks.${hostName}.garageS3.tuple}/storage-${hostName}";
-      backendOptions = [ "no-ssl" ];
+      backend = "s3c://${cluster.config.links.garageS3.hostname}/storage-${hostName}";
     };
   };
 }
