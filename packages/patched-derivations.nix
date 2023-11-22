ce7654740a59755af3f28386f4d9f1067a200df7
Max <max@privatevoid.net>
2023-11-22 23:54:50 +0100

    packages/tempo: 2.2.1 -> 2.3.0

--- packages/patched-derivations.nix
+++ packages/patched-derivations.nix
@@ -105,7 +105,7 @@ super: rec {
     ];
   });
 
-  tempo = (super.tempo.override { buildGoModule = super.buildGo119Module; }).overrideAttrs (_: {
+  tempo = (super.tempo.override { buildGoModule = super.buildGo121Module; }).overrideAttrs (_: {
     version = builtins.substring 1 (-1) pins.tempo.version;
     src = super.npins.mkSource pins.tempo;
     subPackages = [ "cmd/tempo" ];
