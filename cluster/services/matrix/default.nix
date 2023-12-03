cb8744b99ae5225a28479436027602fb453562d6
Max <max@privatevoid.net>
2023-12-03 23:17:12 +0100

    cluster/services/matrix: add dns records

--- cluster/services/matrix/default.nix
+++ cluster/services/matrix/default.nix
@@ -1,4 +1,4 @@
-{ depot, ... }:
+{ config, depot, ... }:
 
 {
   services.matrix = {
@@ -15,5 +15,16 @@
   monitoring.blackbox.targets.matrix = {
     address = "https://matrix.${depot.lib.meta.domain}/_matrix/federation/v1/version";
     module = "https2xx";
+  };
+
+  dns.records = let
+    homeserverAddrs = map
+      (node: depot.hours.${node}.interfaces.primary.addrPublic)
+      config.services.matrix.nodes.homeserver;
+  in {
+    matrix.target = homeserverAddrs;
+    chat.target = homeserverAddrs;
+    stun.target = homeserverAddrs;
+    turn.target = homeserverAddrs;
   };
 }
