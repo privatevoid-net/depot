9abd4b6c0a867afa4dd6ea4ee6e75141227a395c
Max <max@privatevoid.net>
2023-12-03 23:39:58 +0100

    cluster/services/attic: add dns records

--- cluster/services/attic/default.nix
+++ cluster/services/attic/default.nix
@@ -1,3 +1,5 @@
+{ config, depot, ... }:
+
 {
   services.attic = {
     nodes = {
@@ -17,5 +19,14 @@
     buckets.attic = {
       allow.attic = [ "read" "write" ];
     };
+  };
+
+  dns.records = let
+    serverAddrs = map
+      (node: depot.hours.${node}.interfaces.primary.addrPublic)
+      config.services.attic.nodes.server;
+  in {
+    cache-api.target = serverAddrs;
+    cache.target = serverAddrs;
   };
 }
