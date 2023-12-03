38d22c1964a5464821063184faad918d7023e33c
Max <max@privatevoid.net>
2023-12-03 23:13:08 +0100

    cluster/services/warehouse: add dns records

--- cluster/services/warehouse/default.nix
+++ cluster/services/warehouse/default.nix
@@ -1,6 +1,12 @@
+{ config, depot, ... }:
+
 {
   services.warehouse = {
     nodes.host = [ "VEGAS" ];
     nixos.host = [ ./host.nix ];
   };
+
+  dns.records.warehouse.target = map
+    (node: depot.hours.${node}.interfaces.primary.addrPublic)
+    config.services.warehouse.nodes.host;
 }
