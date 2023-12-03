3cbb366b88c3d4ffc0956482ea4dbc8c82891364
Max <max@privatevoid.net>
2023-12-03 23:11:50 +0100

    cluster/services/forge: add dns records

--- cluster/services/forge/default.nix
+++ cluster/services/forge/default.nix
@@ -1,6 +1,12 @@
+{ config, depot, ... }:
+
 {
   services.forge = {
     nodes.server = [ "VEGAS" ];
     nixos.server = ./server.nix;
   };
+
+  dns.records.forge.target = map
+    (node: depot.hours.${node}.interfaces.primary.addrPublic)
+    config.services.forge.nodes.server;
 }
