d48c6fb9865b231d4a40a88f691eb4f6558b333a
Max <max@privatevoid.net>
2023-12-03 23:28:51 +0100

    cluster/services/object-storage: add dns records

--- cluster/services/object-storage/default.nix
+++ cluster/services/object-storage/default.nix
@@ -1,4 +1,4 @@
-{ depot, ... }:
+{ config, depot, ... }:
 
 {
   services.object-storage = {
@@ -9,5 +9,15 @@
   monitoring.blackbox.targets.object-storage = {
     address = "https://object-storage.${depot.lib.meta.domain}/minio/health/live";
     module = "https2xx";
+  };
+
+  dns.records = let
+    serverAddrs = map
+      (node: depot.hours.${node}.interfaces.primary.addrPublic)
+      config.services.object-storage.nodes.host;
+  in {
+    object-storage.target = serverAddrs;
+    "console.object-storage".target = serverAddrs;
+    cdn.target = serverAddrs;
   };
 }
