82bf6c028a8601587e8d3cea1ce2701005ef5a3a
Max <max@privatevoid.net>
2023-12-05 22:55:07 +0100

    cluster/services/nextcloud: nextcloud26 -> nextcloud27

--- cluster/services/nextcloud/host.nix
+++ cluster/services/nextcloud/host.nix
@@ -18,7 +18,7 @@ in
     };
   };
   services.nextcloud = {
-    package = pkgs.nextcloud26;
+    package = pkgs.nextcloud27;
     enable = true;
     https = true;
     hostName = "storage.${depot.lib.meta.domain}";
