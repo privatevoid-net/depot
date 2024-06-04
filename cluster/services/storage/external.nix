d8c0fd5ea2cfe90338fe0c0a1e6c617e01dbf5f3
Max <max@privatevoid.net>
2024-06-04 16:14:33 +0200

    cluster/services/storage: use s3v4 signatures for external storage

--- cluster/services/storage/external.nix
+++ cluster/services/storage/external.nix
@@ -9,7 +9,7 @@ in
     fileSystems.external = {
       mountpoint = "/srv/storage";
       authFile = ./secrets/external-storage-auth-${hostName}.age;
-      backend = "s3c://${cluster.config.links.garageS3.hostname}/storage-${hostName}";
+      backend = "s3c4://${cluster.config.links.garageS3.hostname}/storage-${hostName}";
       backendOptions = [ "disable-expect100" ];
     };
   };
