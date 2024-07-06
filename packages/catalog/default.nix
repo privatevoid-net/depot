8aaa15dd16718cb2be92e33e4d965747027ef44a
Max <max@privatevoid.net>
2024-07-06 22:26:21 +0200

    packages/catalog: add packages and pins

--- packages/catalog/default.nix
+++ packages/catalog/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./checks.nix
+    ./packages.nix
   ];
 }
---
+++ packages/catalog/packages.nix
@@ -0,0 +1,29 @@
+{ lib, ... }:
+
+let
+  pins = import ../sources;
+in
+
+{
+  perSystem = { config, ... }: {
+    catalog.depot = {
+      packages = lib.mapAttrs (name: package: {
+        description = package.meta.description or "Package: ${name}";
+        actions = lib.mkMerge [
+          {
+            build = {
+              description = "Build this package.";
+              command = "nix build -L '${builtins.unsafeDiscardStringContext package.drvPath}^*'";
+            };
+          }
+          (lib.mkIf (pins ? ${name}) {
+            updatePin = {
+              description = "Update this package's source pin.";
+              command = "${lib.getExe config.packages.pin} update ${name}";
+            };
+          })
+        ];
+      }) config.packages;
+    };
+  };
+}
