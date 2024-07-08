804e7b03634c96b11ae5dbf9f95feefbcd51f455
Max <max@privatevoid.net>
2024-07-08 16:17:47 +0200

    cluster/services/cachix-deploy-agent: use cluster secrets

--- cluster/services/cachix-deploy-agent/default.nix
+++ cluster/services/cachix-deploy-agent/default.nix
@@ -1,6 +1,10 @@
 {
-  services.cachix-deploy-agent = {
+  services.cachix-deploy-agent = { config, ... }: {
     nodes.agent = [ "checkmate" "grail" "prophet" "VEGAS" "thunderskin" ];
     nixos.agent = ./agent.nix;
+    secrets.token = {
+      nodes = config.nodes.agent;
+      shared = false;
+    };
   };
 }
