ebc9b88c8d615c550d67277ece830cb0ab7856ba
Max <max@privatevoid.net>
2024-07-20 23:52:16 +0200

    cluster/services/frangiclave: some cluster stuff

--- cluster/services/frangiclave/default.nix
+++ cluster/services/frangiclave/default.nix
@@ -1,13 +1,21 @@
+{ config, ... }:
+
 {
   services.frangiclave = {
     nodes = {
-      server = [ "VEGAS" "grail" "prophet" ]; # 3 reliable nodes
+      server = [ "VEGAS" "grail" "prophet" ];
+      cluster = config.services.frangiclave.nodes.server;
       agent = []; # all nodes, for vault-agent, secret templates, etc.
+    };
+    meshLinks = {
+      server.link.protocol = "http";
+      cluster.link.protocol = "http";
     };
     nixos = {
       server = [
         ./server.nix
       ];
+      cluster = [];
       agent = [];
     };
   };
--- cluster/services/frangiclave/server.nix
+++ cluster/services/frangiclave/server.nix
@@ -1,8 +1,23 @@
-{ depot, ... }:
+{ cluster, config, depot, ... }:
+
+let
+  apiLink = cluster.config.hostLinks.${config.networking.hostName}.frangiclave-server;
+  clusterLink = cluster.config.hostLinks.${config.networking.hostName}.frangiclave-cluster;
+in
 
 {
   services.vault = {
     enable = true;
     package = depot.packages.openbao;
+    address = apiLink.tuple;
+    extraConfig = /*hcl*/ ''
+      api_addr = "${apiLink.url}"
+      cluster_addr = "${clusterLink.url}"
+    '';
+
+    storageBackend = "raft";
+    storageConfig = /*hcl*/ ''
+      node_id = "x${builtins.hashString "sha256" "frangiclave-node-${config.networking.hostName}"}"
+    '';
   };
 }
