fa0d6f046b11397bf7c80a2798e1225d12189397
Max <max@privatevoid.net>
2024-07-23 19:02:11 +0200

    cluster/lib: introduce testConfig

--- cluster/lib/testing.nix
+++ cluster/lib/testing.nix
@@ -1,9 +1,15 @@
 { lib, ... }:
 
 {
-  options.simulacrum = lib.mkOption {
-    description = "Whether we are in the Simulacrum.";
-    type = lib.types.bool;
-    default = false;
+  options = {
+    simulacrum = lib.mkOption {
+      description = "Whether we are in the Simulacrum.";
+      type = lib.types.bool;
+      default = false;
+    };
+    testConfig = lib.mkOption {
+      type = lib.types.attrs;
+      readOnly = true;
+    };
   };
 }
