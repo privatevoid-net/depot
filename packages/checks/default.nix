f3039ec4027a775aa8ee2cfa0acc114f6dbe0421
Max <max@privatevoid.net>
2024-08-03 01:33:22 +0200

    checks/garage: drop

--- packages/checks/default.nix
+++ packages/checks/default.nix
@@ -15,12 +15,6 @@ in
         inherit (config) cluster;
       };
 
-      garage = pkgs.callPackage ./garage.nix {
-        inherit (self'.packages) garage consul;
-        inherit (self) nixosModules;
-        inherit (config) cluster;
-      };
-
       ipfs-cluster-upgrade = pkgs.callPackage ./ipfs-cluster-upgrade.nix {
         inherit (self) nixosModules;
         previous = timeMachine.preUnstable;
