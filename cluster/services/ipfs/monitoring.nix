e1b53161c3efcfbead0e7f6abe3cdee5c6cc3dea
Max <max@privatevoid.net>
2024-08-23 02:50:43 +0200

    cluster/services/ipfs: use correct tempo address

--- cluster/services/ipfs/monitoring.nix
+++ cluster/services/ipfs/monitoring.nix
@@ -9,7 +9,7 @@ in
     environment = {
       OTEL_TRACES_EXPORTER = "otlp";
       OTEL_EXPORTER_OTLP_PROTOCOL = "grpc";
-      OTEL_EXPORTER_OTLP_ENDPOINT = cluster.config.links.tempo-otlp-grpc.url;
+      OTEL_EXPORTER_OTLP_ENDPOINT = cluster.config.ways.ingest-traces-otlp.url;
       OTEL_TRACES_SAMPLER = "parentbased_traceidratio";
       OTEL_TRACES_SAMPLER_ARG = "0.50";
     };
