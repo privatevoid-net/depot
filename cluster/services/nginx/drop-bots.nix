df8e87564e3d8b18b0ef545d46d6a30ccb6ecc46
Max <max@privatevoid.net>
2025-03-04 20:19:51 +0100

    cluster/services/nginx: drop repeat offenders with XDP

--- cluster/services/nginx/drop-bots.nix
+++ cluster/services/nginx/drop-bots.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ lib, ... }:
 
 {
   options.services.nginx.virtualHosts = lib.mkOption {
@@ -10,23 +10,37 @@
       '';
     });
   };
-  config.services.nginx.appendHttpConfig = ''
-    map $http_user_agent $drop_ua {
-      default 0;
-      ~*Amazonbot 1;
-      ~*Bytespider 1;
-      ~*ClaudeBot 1;
-      ~*DataForSeoBot 1;
-      ~*DotBot 1;
-      ~*GPTBot 1;
-      ~*Googlebot 1;
-      ~*ImagesiftBot 1;
-      ~*MJ12Bot 1;
-      ~*OAI-SearchBot 1;
-      ~*SemrushBot 1;
-      ~*YandexBot 1;
-      ~*bingbot 1;
-      ~*meta-externalagent 1;
-    }
-  '';
+
+  config = {
+    services.nginx.appendHttpConfig = ''
+      map $http_user_agent $drop_ua {
+        default 0;
+        ~*Amazonbot 1;
+        ~*Bytespider 1;
+        ~*ClaudeBot 1;
+        ~*DataForSeoBot 1;
+        ~*DotBot 1;
+        ~*GPTBot 1;
+        ~*Googlebot 1;
+        ~*ImagesiftBot 1;
+        ~*MJ12Bot 1;
+        ~*OAI-SearchBot 1;
+        ~*SemrushBot 1;
+        ~*YandexBot 1;
+        ~*bingbot 1;
+        ~*meta-externalagent 1;
+        }
+    '';
+
+    environment.etc."fail2ban/filter.d/nginx-drop-status-444.conf".text = ''
+      [Definition]
+      journalmatch = _SYSTEMD_UNIT=nginx.service + _COMM=nginx + SYSLOG_IDENTIFIER=nginx_access
+      failregex = host=.* remote_addr=<ADDR> .* status=444 .*
+    '';
+
+    services.fail2ban.jails.nginx.settings = {
+      filter = "nginx-drop-status-444";
+      banaction = "xdp";
+    };
+  };
 }
