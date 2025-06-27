70aca6a548ce496132c3ba19e409bff2d2931d26
Max <max@privatevoid.net>
2025-06-27 23:27:12 +0200

    cluster/services/monitoring: move blackbox to alloy

--- cluster/services/monitoring/blackbox.nix
+++ cluster/services/monitoring/blackbox.nix
@@ -1,23 +1,10 @@
-{ cluster, config, lib, depot, ... }:
+{ cluster, config, lib, depot, pkgs, ... }:
 
 let
-  inherit (lib) flip pipe mapAttrsToList range recursiveUpdate substring;
-
+  inherit (lib) pipe mapAttrsToList recursiveUpdate substring;
   inherit (depot.lib.meta) domain;
-  inherit (cluster.config) vars;
 
-  mapTargets = mapAttrsToList (name: value: value // { name = "default/${name}"; });
-
-  mkSecretTargets = amount: map (flip pipe [
-    toString
-    (num: let
-      prefix = "SECRET_MONITORING_BLACKBOX_TARGET_${num}";
-    in {
-      name = "secret/\${${prefix}_NAME}";
-      module = "\${${prefix}_MODULE}";
-      address = "\${${prefix}_ADDRESS}";
-    })
-  ]) (range 1 1);
+  mapTargets = mapAttrsToList (name: value: value // { inherit name; });
 
   probeId = pipe "blackbox-probe-${domain}-${config.networking.hostName}" [
     (builtins.hashString "md5")
@@ -30,80 +17,120 @@ let
     User-Agent = probeUserAgent;
   };
 
-  relabel = from: to: {
-    source_labels = [ from ];
-    target_label = to;
-  };
-in
-
-{
-  services.grafana-agent.settings.integrations.blackbox = {
-    enabled = true;
-    instance = config.networking.hostName;
-    scrape_interval = "600s";
-    relabel_configs = [
-      (relabel "__param_module" "module")
-      (relabel "__param_target" "target")
-      {
-        target_label = "probe_id";
-        replacement = probeId;
-      }
-    ];
-    blackbox_config.modules = rec {
-      http2xx = {
-        prober = "http";
-        http = {
-          headers = defaultHttpHeaders;
-          preferred_ip_protocol = "ip4";
-        };
+  blackboxModules = rec {
+    http2xx = {
+      prober = "http";
+      http = {
+        headers = defaultHttpHeaders;
+        preferred_ip_protocol = "ip4";
       };
-      https2xx = recursiveUpdate http2xx {
-        http.fail_if_not_ssl = true;
+    };
+    https2xx = recursiveUpdate http2xx {
+      http.fail_if_not_ssl = true;
+    };
+    tcpConnect = {
+      prober = "tcp";
+      tcp = {
+        preferred_ip_protocol = "ip4";
       };
-      tcpConnect = {
-        prober = "tcp";
-        tcp = {
-          preferred_ip_protocol = "ip4";
-        };
-      };
-      ircConnect = recursiveUpdate tcpConnect {
-        tcp.query_response = [
-          { send = "NICK probe"; }
-          { send = "USER probe probe probe :${probeUserAgent}"; }
-          { send = "PING probe${probeId}"; }
-          { expect = "PONG .* :probe${probeId}"; }
-          { send = "QUIT"; }
-        ];
-      };
-      ircsConnect = recursiveUpdate ircConnect {
-        tcp.tls = true;
-      };
-      nextcloudStatus = recursiveUpdate https2xx {
-        http = {
-          fail_if_body_not_matches_regexp = [
-            ''"installed":true''
-            ''"maintenance":false''
-          ];
-        };
-      };
-      sshConnect = recursiveUpdate tcpConnect {
-        tcp.query_response = [
-          { expect = "^SSH-2.0"; }
-          { send = "SSH-2.0-PrivateVoidProbe_${probeId}"; }
+    };
+    ircConnect = recursiveUpdate tcpConnect {
+      tcp.query_response = [
+        { send = "NICK probe"; }
+        { send = "USER probe probe probe :${probeUserAgent}"; }
+        { send = "PING probe${probeId}"; }
+        { expect = "PONG .* :probe${probeId}"; }
+        { send = "QUIT"; }
+      ];
+    };
+    ircsConnect = recursiveUpdate ircConnect {
+      tcp.tls = true;
+    };
+    nextcloudStatus = recursiveUpdate https2xx {
+      http = {
+        fail_if_body_not_matches_regexp = [
+          ''"installed":true''
+          ''"maintenance":false''
         ];
       };
     };
-    blackbox_targets = let
-      regularTargets = mapTargets cluster.config.monitoring.blackbox.targets;
-      secretTargets = mkSecretTargets 1;
-    in regularTargets ++ secretTargets;
+    sshConnect = recursiveUpdate tcpConnect {
+      tcp.query_response = [
+        { expect = "^SSH-2.0"; }
+        { send = "SSH-2.0-PrivateVoidProbe_${probeId}"; }
+      ];
+    };
   };
+  blackboxConfigFile = pkgs.writeText "blackbox-config.json" (builtins.toJSON {
+    modules = blackboxModules;
+  });
 
+  relabelConfigForGroup = group: ''
+    rule {
+      target_label = "probe_id"
+      replacement = ${builtins.toJSON probeId}
+    }
+
+    rule {
+      source_labels = ["__param_module"]
+      target_label = "module"
+    }
+
+    rule {
+      source_labels = ["__param_target"]
+      target_label = "target"
+    }
+
+    rule {
+      source_labels = ["job"]
+      target_label = "target_name"
+      regex = "integrations/blackbox/(.*)"
+      replacement = "$1"
+    }
+
+    rule {
+      target_label = "blackbox_group"
+      replacement = ${builtins.toJSON group}
+    }
+
+    rule {
+      source_labels = ["job"]
+      target_label = "job"
+      regex = "integrations/blackbox/(.*)"
+      replacement = "integrations/blackbox/${group}/$1"
+    }
+  '';
+in
+
+{
   age.secrets = {
-    grafana-agent-blackbox-secret-monitoring.file = ./secrets/secret-monitoring/blackbox.age;
+    blackboxSecretMonitoring.file = ./secrets/secret-monitoring/blackbox.age;
   };
 
-  systemd.services.grafana-agent.serviceConfig = {
-    EnvironmentFile = config.age.secrets.grafana-agent-blackbox-secret-monitoring.path;
+  systemd.services.alloy.serviceConfig.EnvironmentFile = [ config.age.secrets.blackboxSecretMonitoring.path ];
+
+  services.alloy.metrics.integrations = {
+    blackbox = {
+      exporter = "blackbox";
+      name = "blackbox/default";
+      scrapeInterval = 600;
+      settings = {
+        config_file = blackboxConfigFile;
+        targets = mapTargets cluster.config.monitoring.blackbox.targets;
+      };
+      relabelConfigText = relabelConfigForGroup "default";
+    };
+
+    secretMonitoring = {
+      exporter = "blackbox";
+      name = "blackbox/secret";
+      scrapeInterval = 600;
+      labels.probe_id = probeId;
+      settings.config_file = blackboxConfigFile;
+      configText = ''
+        targets = encoding.from_json(coalesce(encoding.from_base64(sys.env("BLACKBOX_SECRET_MONITORING_CONFIG")),"[]"))
+      '';
+      relabelConfigText = relabelConfigForGroup "secret";
+    };
   };
 }
