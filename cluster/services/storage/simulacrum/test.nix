6f41520ec1796bfbbafa8151fb5a766953ecc7fc
Max <max@privatevoid.net>
2025-07-20 03:42:24 +0200

    cluster/services/storage: fix test for garage 0.9, test upgrade

--- cluster/services/storage/simulacrum/test.nix
+++ cluster/services/storage/simulacrum/test.nix
@@ -4,27 +4,55 @@ let
   inherit (cluster.config.services.storage) nodes;
 
   firstGarageNode = lib.elemAt nodes.garage 0;
+
+  getPreviousVersion = currentVersion: let
+    currentMajor = lib.versions.major currentVersion;
+    currentMinor = lib.versions.minor currentVersion;
+    downgrade = num: toString ((lib.toInt num) - 1);
+    previousVersion = if currentMajor == "0"
+      then [ "0" (downgrade currentMinor) ]
+      else if currentMajor == "1" then [ "0" "9" ]
+      else [ (downgrade currentMajor) ];
+    in {
+      version = lib.concatStringsSep "." previousVersion;
+      attr = lib.concatStringsSep "_" previousVersion;
+    };
+
+  useCurrentGarage = { depot, ... }: {
+    services.garage.package = lib.mkForce depot.packages.garage;
+  };
 in
 
 {
-  nodes = lib.genAttrs nodes.garage (node: {
+  nodes = lib.genAttrs nodes.garage (node: { depot, pkgs, ... }: {
     services.garage = {
       layout.initial = lib.genAttrs nodes.garage (_: {
         capacity = lib.mkOverride 51 1000;
       });
+      package = let
+        prev = getPreviousVersion depot.packages.garage.version;
+        attr = "garage_${prev.attr}";
+      in lib.mkOverride 51 (depot.packages.${attr} or pkgs.${attr});
     };
-    specialisation.modifiedLayout = {
-      inheritParentConfig = true;
-      configuration = {
-        services.garage = {
-          layout.initial.${firstGarageNode}.capacity = lib.mkForce 2000;
+    specialisation = {
+      upgrade = {
+        inheritParentConfig = true;
+        configuration = useCurrentGarage;
+      };
+      modifiedLayout = {
+        inheritParentConfig = true;
+        configuration = {
+          imports = [ useCurrentGarage ];
+          services.garage = {
+            layout.initial.${firstGarageNode}.capacity = lib.mkForce 2000;
+          };
+          system.ascensions.garage-layout.incantations = lib.mkForce (i: [
+            (i.runGarage ''
+              garage layout assign -z eu-central -c 2000GB "$(garage node id -q | cut -d@ -f1)"
+              garage layout apply --version 2
+            '')
+          ]);
         };
-        system.ascensions.garage-layout.incantations = lib.mkForce (i: [
-          (i.runGarage ''
-            garage layout assign -z eu-central -c 2000 "$(garage node id -q | cut -d@ -f1)"
-            garage layout apply --version 2
-          '')
-        ]);
       };
     };
   });
@@ -43,17 +71,31 @@ in
       for node in nodes:
         node.wait_until_fails("garage status | grep 'NO ROLE ASSIGNED'")
 
+      for node in nodes:
+        node.wait_until_succeeds("garage layout show | grep -w 'Current cluster layout version: 1'")
+
+    with subtest("should upgrade from previous version"):
+      for node in nodes:
+        node.wait_until_succeeds('test "$(systemctl list-jobs | wc -l)" -eq 1')
+
+      from concurrent.futures import ThreadPoolExecutor
+
+      with ThreadPoolExecutor(max_workers=len(nodes)) as ex:
+        for node in nodes:
+          ex.submit(node.succeed, "/run/booted-system/specialisation/upgrade/bin/switch-to-configuration test")
+        ex.shutdown()
+
     with subtest("should apply new layout with ascension"):
       for node in nodes:
         node.wait_until_succeeds('test "$(systemctl list-jobs | wc -l)" -eq 1')
 
       for node in nodes:
-        node.succeed("/run/current-system/specialisation/modifiedLayout/bin/switch-to-configuration test")
+        node.succeed("/run/booted-system/specialisation/modifiedLayout/bin/switch-to-configuration test")
 
       for node in nodes:
-        node.wait_until_succeeds("garage layout show | grep -w 2000")
-        assert "1" in node.succeed("garage layout show | grep -w 2000 | wc -l")
-        assert "2" in node.succeed("garage layout show | grep -w 1000 | wc -l")
+        node.wait_until_succeeds("garage layout show | grep -w 'eu-central  *2\\.0 TB'")
+        assert "1" in node.succeed("garage layout show | grep -w 'eu-central  *2\\.0 TB' | wc -l")
+        assert "2" in node.succeed("garage layout show | grep -w 'eu-central  *1073\\.7 GB' | wc -l")
 
     consulConfig = json.loads(garage1.succeed("cat /etc/consul.json"))
     addr = consulConfig["addresses"]["http"]
@@ -75,9 +117,9 @@ in
         node.wait_until_fails("garage status | grep 'NO ROLE ASSIGNED'")
 
       for node in nodes:
-        node.wait_until_succeeds("garage layout show | grep -w 2000")
-        assert "1" in node.succeed("garage layout show | grep -w 2000 | wc -l")
-        assert "${toString ((lib.length nodes.garage) - 1)}" in node.succeed("garage layout show | grep -w 1000 | wc -l")
+        node.wait_until_succeeds("garage layout show | grep -w 'eu-central  *2\\.0 TB'")
+        assert "1" in node.succeed("garage layout show | grep -w 'eu-central  *2\\.0 TB' | wc -l")
+        assert "${toString ((lib.length nodes.garage) - 1)}" in node.succeed("garage layout show | grep -w 'eu-central  *1000\\.0 GB' | wc -l")
 
     with subtest("should create specified buckets and keys"):
       for node in nodes:
@@ -87,7 +129,7 @@ in
 
     with subtest("should delete unspecified keys"):
       garage1.succeed("garage bucket create unwantedbucket")
-      garage1.succeed("garage key new --name unwantedkey")
+      garage1.succeed("garage key create unwantedkey || garage key new --name unwantedkey")
       garage1.succeed(f"{setEnv} consul kv put services/incandescence/providers/garage/formulae/key/unwantedkey/alive true")
       garage1.succeed(f"{setEnv} consul kv put services/incandescence/providers/garage/formulae/bucket/unwantedbucket/alive true")
       garage1.succeed("systemctl restart garage.service")
@@ -102,4 +144,3 @@ in
       garage1.fail("garage bucket list | grep unwantedbucket")
   '';
 }
-
