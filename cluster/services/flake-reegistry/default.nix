ebb516a6c552a8344e220a21f862aeee16d36345
Max <max@privatevoid.net>
2025-07-25 22:13:04 +0200

    cluster/services/flake-registry: use forService

--- cluster/services/flake-reegistry/default.nix
+++ cluster/services/flake-reegistry/default.nix
@@ -1,36 +1,38 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
-  ways.registry.static = { depot, pkgs, ... }: pkgs.writeTextDir "flake-registry.json" (let
-    flakes = {
-      depot = {
-        type = "tarball";
-        url = "https://forge.${depot.lib.meta.domain}/${depot.lib.meta.domain}/depot/archive/master.tar.gz";
-      };
-      depot-nixpkgs = {
-        type = "github";
-        owner = "NixOS";
-        repo = "nixpkgs";
-        inherit (depot.inputs.nixpkgs.sourceInfo) rev narHash lastModified;
-      };
-      blank = {
-        type = "github";
-        owner = "divnix";
-        repo = "blank";
-        inherit (depot.inputs.blank.sourceInfo) rev narHash lastModified;
-      };
-    } // import ./extra-flakes.nix;
-  in builtins.toJSON {
-    version = 2;
-    flakes = lib.pipe flakes [
-      (lib.attrsToList)
-      (map (f: {
-        from = {
-          type = "indirect";
-          id = f.name;
+  ways = config.lib.forService "flake-registry" {
+    registry.static = { depot, pkgs, ... }: pkgs.writeTextDir "flake-registry.json" (let
+      flakes = {
+        depot = {
+          type = "tarball";
+          url = "https://forge.${depot.lib.meta.domain}/${depot.lib.meta.domain}/depot/archive/master.tar.gz";
         };
-        to = f.value;
-      }))
-    ];
-  });
+        depot-nixpkgs = {
+          type = "github";
+          owner = "NixOS";
+          repo = "nixpkgs";
+          inherit (depot.inputs.nixpkgs.sourceInfo) rev narHash lastModified;
+        };
+        blank = {
+          type = "github";
+          owner = "divnix";
+          repo = "blank";
+          inherit (depot.inputs.blank.sourceInfo) rev narHash lastModified;
+        };
+      } // import ./extra-flakes.nix;
+    in builtins.toJSON {
+      version = 2;
+      flakes = lib.pipe flakes [
+        (lib.attrsToList)
+        (map (f: {
+          from = {
+            type = "indirect";
+            id = f.name;
+          };
+          to = f.value;
+        }))
+      ];
+    });
+  };
 }
