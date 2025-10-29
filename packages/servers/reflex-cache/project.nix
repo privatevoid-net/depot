26c81439572727ea44da69af080af321ecf16472
Max <max@privatevoid.net>
2025-10-29 22:32:47 +0100

    packages/reflex-cache: use downgraded py-multiaddr 0.0.9
    i love when nixpkgs maintainers blindly bump packages

--- packages/servers/reflex-cache/project.nix
+++ packages/servers/reflex-cache/project.nix
@@ -6,7 +6,15 @@
       poetry-core
       requests-unixsocket
       py-multibase
-      py-multiaddr
+      (py-multiaddr.overrideAttrs rec {
+        version = "0.0.9";
+        src = pkgs.fetchFromGitHub {
+          owner = "multiformats";
+          repo = "py-multiaddr";
+          tag = "v${version}";
+          hash = "sha256-cGM7iYQPP+UOkbTxRhzuED0pkcydFCO8vpx9wTc0/HI=";
+        };
+      })
     ];
 
     pythonForDev = pkgs.python3.withPackages (lib.const deps);
