27166c1c1bef74cd391852fd3c18af30a7cabb60
Max <max@privatevoid.net>
2025-11-02 20:44:03 +0100

    hosts: fix deprecations

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -9,7 +9,7 @@ let
     modules = [
       host.nixos
       (withSystem host.system ({ config, pkgs, ... }: {
-        nixpkgs.pkgs = assert pkgs.buildPlatform == pkgs.hostPlatform; pkgs // {
+        nixpkgs.pkgs = assert pkgs.stdenv.buildPlatform == pkgs.stdenv.hostPlatform; pkgs // {
           __splicedPackages = pkgs.__splicedPackages // config.shadows;
         };
       }))
