36cdadd183b34ae32384caccbd2354c932e11db3
Max <max@privatevoid.net>
2025-11-07 22:40:16 +0100

    cluster/services/ipfs: remove tracing

--- cluster/services/ipfs/monitoring.nix
+++ cluster/services/ipfs/monitoring.nix
@@ -5,16 +5,6 @@ let
 in
 
 {
-  systemd.services.ipfs = {
-    environment = {
-      OTEL_TRACES_EXPORTER = "otlp";
-      OTEL_EXPORTER_OTLP_PROTOCOL = "grpc";
-      OTEL_EXPORTER_OTLP_ENDPOINT = "${cluster.config.ways.ingest-traces-otlp.url}:443";
-      OTEL_TRACES_SAMPLER = "parentbased_traceidratio";
-      OTEL_TRACES_SAMPLER_ARG = "0.50";
-    };
-  };
-
   services.alloy.metrics.targets.ipfs = {
     address = links.ipfsMetrics.tuple;
     metricsPath = links.ipfsMetrics.path;
