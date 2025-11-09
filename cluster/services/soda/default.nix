443f0c87bb45167c312fa9ea24c74ffa8731c1b3
Max <max@privatevoid.net>
2025-11-09 23:21:12 +0100

    cluster/services/soda: host schizo.cooking

--- cluster/services/soda/default.nix
+++ cluster/services/soda/default.nix
@@ -1,9 +1,10 @@
-{ depot, ... }:
+{ config, depot, ... }:
 
 {
   services.soda = {
     nodes.host = [ "VEGAS" ];
     nixos.host = ./host.nix;
+    meshLinks.host.quickie.link.protocol = "http";
   };
 
   monitoring.blackbox.targets.soda-machine = {
@@ -14,5 +15,14 @@
   dns.records = {
     soda.target = [ depot.hours.VEGAS.interfaces.primary.addrPublic ];
     "soda.int".target = [ "10.10.2.206" ];
+  };
+
+  ways = let
+    host = builtins.head config.services.forge.nodes.server;
+  in config.lib.forService "soda" {
+    schizo = {
+      target = config.hostLinks.${host}.quickie.url;
+      domainSuffix = "cooking";
+    };
   };
 }
--- cluster/services/soda/host.nix
+++ cluster/services/soda/host.nix
@@ -1,6 +1,11 @@
-{ depot, ... }:
+{ cluster, config, depot, ... }:
 
 {
+  links = {
+    quickieInternal.protocol = "http";
+    quickie.protocol = "http";
+  };
+
   containers.soda = {
     path = depot.nixosConfigurations.soda.config.system.build.toplevel;
     privateNetwork = true;
@@ -21,7 +26,83 @@
     };
   };
 
-  systemd.services."container@soda".after = [ "libvirtd.service" "sys-devices-virtual-net-vmdefault.device" ];
+  systemd = {
+    services = {
+      "container@soda".after = [ "libvirtd.service" "sys-devices-virtual-net-vmdefault.device" ];
+
+      quickie = {
+        serviceConfig = {
+          ExecStart = "${depot.packages.quickie}/bin/quickie -b ${config.links.quickieInternal.ipv4} -p ${config.links.quickieInternal.portStr} -m /tmp/resources -o /tmp -c /resources/asylum-v1.css";
+          WorkingDirectory = "/tmp";
+
+          CPUQuota = "10%";
+          CapabilityBoundingSet = "";
+          DevicePolicy = "closed";
+          DynamicUser = true;
+          IPAddressAllow = "${config.links.quickieInternal.ipv4}/32";
+          IPAddressDeny = "any";
+          LockPersonality = true;
+          MemoryDenyWriteExecute = true;
+          MemoryMax = "16M";
+          MemorySwapMax = "32M";
+          NoNewPrivileges = true;
+          PrivateDevices = true;
+          PrivateNetwork = true;
+          PrivateTmp = true;
+          ProcSubset = "pid";
+          ProtectClock = true;
+          ProtectControlGroups = true;
+          ProtectHome = true;
+          ProtectHostname = true;
+          ProtectKernelLogs = true;
+          ProtectKernelModules = true;
+          ProtectKernelTunables = true;
+          ProtectProc = "invisible";
+          RestrictAddressFamilies = "AF_INET";
+          RestrictNamespaces = true;
+          RestrictRealtime = true;
+          SocketBindAllow = "ipv4:tcp:${config.links.quickieInternal.portStr}";
+          SocketBindDeny = "any";
+          SystemCallArchitectures = "native";
+          SystemCallFilter = [ "@system-service" "~@privileged" "~@resources" ];
+          TasksMax = 1;
+          UMask = "0077";
+
+          InaccessiblePaths = [
+            "/boot"
+            "/nix/var"
+            "/srv"
+            "/var"
+          ];
+          BindReadOnlyPaths = [ "/srv/storage/www/schizo.cooking:/tmp/resources" ];
+        };
+      };
+
+      quickie-proxy = {
+        bindsTo = [ "quickie.service" ];
+        after = [ "quickie.service" ];
+        unitConfig.JoinsNamespaceOf = "quickie.service";
+        serviceConfig = {
+          ExecStart = "${config.systemd.package}/lib/systemd/systemd-socket-proxyd ${config.links.quickieInternal.tuple}";
+          DynamicUser = true;
+          IPAddressAllow = "${config.links.quickieInternal.ipv4}/32";
+          IPAddressDeny = "any";
+          MemoryMax = "64M";
+          MemorySwapMax = "128M";
+          NoNewPrivileges = true;
+          PrivateDevices = true;
+          PrivateNetwork = true;
+          PrivateTmp = true;
+          TasksMax = 16;
+        };
+      };
+    };
+
+    sockets.quickie-proxy = {
+      wantedBy = [ "sockets.target" ];
+      socketConfig.ListenStream = "${cluster.config.hostLinks.${config.networking.hostName}.quickie.tuple}";
+    };
+  };
 
   networking.nat.forwardPorts = [
     {
