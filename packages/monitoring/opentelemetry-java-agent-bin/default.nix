ec4f42da5f30871d4fd862f1d281b7f6a146ebae
Max <max@privatevoid.net>
2025-11-25 00:40:50 +0100

    packages/opentelemetry-java-agent-bin: drop

--- packages/monitoring/opentelemetry-java-agent-bin/default.nix
+++
@@ -1,8 +0,0 @@
-{ fetchurl }:
-
-fetchurl rec {
-  name = "opentelemetry-java-agent-${meta.version}.jar";
-  meta.version = "1.19.1";
-  url = "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v${meta.version}/opentelemetry-javaagent.jar";
-  sha256 = "sha256-f1kc0eqBrK+QmlRaZRiJq5OAKa2wrtTyLeBN8uK6698=";
-}
