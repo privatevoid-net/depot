74a5c67c8446157301293d47cec90f86ca2e13c9
Max <max@privatevoid.net>
2025-11-25 00:31:37 +0100

    hosts: remove unnecessary DNS aliasing

--- hosts/tools.nix
+++ hosts/tools.nix
@@ -1,26 +1,9 @@
 {
   dns = rec {
-    findSvc = name: [
-      "any.${name}"
-      "local.${name}"
-      "tunnel.${name}"
-      "wired.${name}"
-      "wireless.${name}"
-      "*.if.${name}"
-    ];
-    findResolve = list: dnameResolve (append "find" list) ++ append "f.void" list;
-    dnameResolve = list: append "private.void" list ++ append "privatevoid.net" list;
-    vpnResolve = list: dnameResolve (append "vpn" list);
-    llmnrResolve = append "local";
+    dnameResolve = list: append "privatevoid.net" list;
     append = part: map (x: "${x}.${part}");
     portMap = port: map (x: "[${x}]:${builtins.toString port}");
     as = x: [x];
-
-    clientResolve = x: [x] ++
-      findResolve (findSvc x) ++
-      vpnResolve [x] ++
-      llmnrResolve [x];
-
     subResolve = name: sub: [name] ++ dnameResolve ["${name}.${sub}"];
   };
   ssh = {
