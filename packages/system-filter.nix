1a951276543be28d5827c3a014f11a9a949ae5a3
Max <max@privatevoid.net>
2025-12-08 21:44:46 +0100

    packages/keycloak: also build for aarch64-linux

--- packages/system-filter.nix
+++ packages/system-filter.nix
@@ -1,6 +1,6 @@
 {
   packages = {
-    keycloak = [ "x86_64-linux" ];
+    keycloak = [ "aarch64-linux" "x86_64-linux" ];
     out-of-your-element = [ "x86_64-linux" ];
     prometheus-jitsi-exporter = [ "aarch64-linux" ];
   };
