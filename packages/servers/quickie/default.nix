8c2335b0f81fe8277dbf36138bb6b3413470fbdb
Max <max@privatevoid.net>
2025-12-28 16:53:03 +0100

    packages/quickie: 1.0 -> 1.1

--- packages/servers/quickie/default.nix
+++ packages/servers/quickie/default.nix
@@ -2,14 +2,18 @@
 
 stdenv.mkDerivation {
   pname = "quickie";
-  version = "1.0";
+  version = "1.1";
 
   src = fetchFromGitHub {
     owner = "schizo-org";
     repo = "schizo.cooking";
-    rev = "bec0465266989a2f7520adffa30d956caffb7daf";
-    sha256 = "sha256-uNsKuhWeLo6lEAGA0V8xLwQFvK3ya/YwVkLB+N8SNOc=";
+    rev = "690349a92f60dd3dd10af3bf26c1f99ee83349b8";
+    sha256 = "sha256-1RzDsISk6E+XUxVY9fHPSjOExzkLykhtpaBeFHpFEb4=";
   };
+  
+  postPatch = ''
+    substituteInPlace quickie/quickie.c --replace-fail IN_NONBLOCK 0
+  '';
 
   makeFlags = [ "quickie" ];
   
