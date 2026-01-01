ea2016d2651e33348e666a4be058d68a0799268f
Max <max@privatevoid.net>
2026-01-01 21:15:17 +0100

    cluster/services/bitwarden: migrate to patroni

--- cluster/services/bitwarden/default.nix
+++ cluster/services/bitwarden/default.nix
@@ -1,9 +1,17 @@
-{ depot, ... }:
+{ config, depot, ... }:
 
 {
   services.bitwarden = {
     nodes.host = [ "VEGAS" ];
     nixos.host = ./host.nix;
+  };
+
+  patroni = config.lib.forService "bitwarden" {
+    databases.vaultwarden = {};
+    users.vaultwarden.locksmith = {
+      nodes = config.services.bitwarden.nodes.host;
+      format = "envFile";
+    };
   };
 
   dns.records.keychain.target = [ depot.hours.VEGAS.interfaces.primary.addrPublic ];
--- cluster/services/bitwarden/host.nix
+++ cluster/services/bitwarden/host.nix
@@ -1,27 +1,31 @@
-{ config, lib, depot, ... }:
+{ cluster, config, depot, ... }:
+let
+  patroni = cluster.config.links.patroni-pg-access;
+in
+
 with depot.lib.nginx;
 {
   links.bitwarden.protocol = "http";
 
+  services.locksmith.waitForSecrets.vaultwarden = [
+    "patroni-vaultwarden"
+  ];
+
   services.nginx.virtualHosts = mappers.mapSubdomains {
     keychain = vhosts.proxy config.links.bitwarden.url;
   };
+
   services.vaultwarden = {
     enable = true;
-    backupDir = "/srv/storage/private/bitwarden/backups";
+    dbBackend = "postgresql";
     config = {
       dataFolder = "/srv/storage/private/bitwarden/data";
       rocketPort = config.links.bitwarden.port;
+      databaseUrl = "postgresql://vaultwarden@${patroni.tuple}/vaultwarden?sslmode=disable";
     };
-    #environmentFile = ""; # TODO: agenix
+    environmentFile = [ "/run/locksmith/patroni-vaultwarden" ];
   };
   systemd.services.vaultwarden.serviceConfig = {
     ReadWriteDirectories = "/srv/storage/private/bitwarden";
-  };
-  systemd.services.backup-vaultwarden = {
-    environment.DATA_FOLDER = lib.mkForce config.services.vaultwarden.config.dataFolder;
-    serviceConfig = {
-      ReadWriteDirectories = "/srv/storage/private/bitwarden";
-    };
   };
 }
