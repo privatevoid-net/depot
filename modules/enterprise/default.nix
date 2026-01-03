674d4598cadc59c032694c3239d8ba969a2d394e
Max <max@privatevoid.net>
2026-01-03 23:29:20 +0100

    modules/enterprise: set networking.domain without mkDefault

--- modules/enterprise/default.nix
+++ modules/enterprise/default.nix
@@ -1,8 +1,8 @@
-{ config, depot, lib, ... }:
+{ config, depot, ... }:
 let
   orgDomain = depot.lib.meta.domain;
   host = config.reflection;
 in {
-  networking.domain = lib.mkDefault "${host.enterprise.subdomain or "services"}.${orgDomain}";
+  networking.domain = "${host.enterprise.subdomain or "services"}.${orgDomain}";
   networking.search = [ config.networking.domain "search.${orgDomain}" ];
 }
