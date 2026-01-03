e65798014a791095e33c6cc72d4d418fd202b771
Max <max@privatevoid.net>
2026-01-03 19:06:42 +0100

    modules/reflection: transpose packages in depot'

--- modules/reflection/default.nix
+++ modules/reflection/default.nix
@@ -1,4 +1,4 @@
-{ config, depot, lib, ... }:
+{ config, depot, lib, pkgs, ... }:
 
 {
   options.reflection = lib.mkOption {
@@ -6,5 +6,14 @@
     type = lib.types.raw;
     readOnly = true;
     default = depot.hours.${config.networking.hostName};
+  };
+
+  config._module.args.depot' = let
+    inherit (pkgs.stdenv.hostPlatform) system;
+  in {
+    packages = depot.packages.${system};
+    inputs = lib.mapAttrs (_: input: {
+      packages = input.packages.${system};
+    }) depot.inputs;
   };
 }
