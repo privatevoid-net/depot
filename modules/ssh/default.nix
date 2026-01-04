e695b70337f529bc86e55be20f2f2ef3704da2bd
Max <max@privatevoid.net>
2026-01-04 14:40:59 +0100

    modules/ssh: generate hostnames of known hosts inline

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -3,12 +3,18 @@ let
   filtered = lib.filterAttrs (_: host: host.ssh.enable) depot.hours;
   idCapable = lib.filterAttrs (_: host: host.ssh.id.publicKey != null) filtered;
   configCapable = lib.filterAttrs (_: host: host.ssh.extraConfig != "") filtered;
-
-  sshHosts = lib.mapAttrs (_: host: host.ssh.id) idCapable;
   sshExtras = lib.mapAttrsToList (_: host: host.ssh.extraConfig) configCapable;
 in {
   programs.ssh = {
-    knownHosts = sshHosts;
+    knownHosts = lib.mapAttrs (name: host: let
+      baseName = lib.toLower name;
+    in {
+      inherit (host.ssh.id) publicKey;
+      hostNames = [
+        baseName
+        "${baseName}.${host.enterprise.subdomain}.${depot.lib.meta.domain}"
+      ];
+    }) idCapable;
     extraConfig = builtins.concatStringsSep "\n" sshExtras;
   };
 }
