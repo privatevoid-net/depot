44a6e166494c08525b1fb50554492eaf5aa49d54
Max <max@privatevoid.net>
2026-01-04 02:14:24 +0100

    modules/networking: unify network configuration

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -3,17 +3,43 @@ let
   inherit (config.reflection) interfaces;
 in
 {
-  networking.interfaces = lib.mkIf (interfaces ? vstub) {
-    ${interfaces.vstub.link} = {
-      virtual = true;
-      ipv4.addresses = [
-        {
-          address = interfaces.vstub.addr;
-          prefixLength = 32;
-        }
-      ];
+  networking = {
+    useDHCP = false;
+    dhcpcd.enable = false;
+    defaultGateway = {
+      address = interfaces.primary.gatewayAddr;
+      interface = interfaces.primary.link;
     };
+    interfaces = lib.mkMerge [
+      {
+        ${interfaces.primary.link} = {
+          ipv4 = {
+            addresses = [
+              {
+                address = interfaces.primary.addr;
+                inherit (interfaces.primary) prefixLength;
+              }
+            ];
+            routes = lib.mkIf (interfaces.primary.prefixLength == 32) [
+              {
+                address = interfaces.primary.gatewayAddr;
+                prefixLength = 32;
+              }
+            ];
+          };
+        };
+      }
+      (lib.mkIf (interfaces ? vstub) {
+        ${interfaces.vstub.link} = {
+          virtual = true;
+          ipv4.addresses = [
+            {
+              address = interfaces.vstub.addr;
+              prefixLength = 32;
+            }
+          ];
+        };
+      })
+    ];
   };
-
-  networking.defaultGateway.interface = interfaces.primary.link;
 }
