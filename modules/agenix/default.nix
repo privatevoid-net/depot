3cce994b81568a1b6332be6c2eb4523b1992286f
Max <max@privatevoid.net>
2026-01-05 20:30:49 +0100

    modules/agenix: run agenixInstall after sops-nix's setupSecrets

--- modules/agenix/default.nix
+++ modules/agenix/default.nix
@@ -1,7 +1,9 @@
-{ depot, ... }:
+{ depot, lib, options, ... }:
 
 {
   imports = [
     depot.inputs.agenix.nixosModules.age
   ];
+
+  system.activationScripts.agenixInstall.deps = lib.mkIf (options ? sops) [ "setupSecrets" ];
 }
