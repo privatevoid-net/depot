204d508cc81c47a0006121d044efd6f54d6f8049
Max <max@privatevoid.net>
2026-04-04 02:02:39 +0200

    cluster/services/search: proxy requests to various other searxng instances over tor

--- cluster/services/search/host.nix
+++ cluster/services/search/host.nix
@@ -1,6 +1,17 @@
 { cluster, config, depot, lib, ... }:
 let
   inherit (config) links;
+  torProxyEngine = { name, domain }: {
+    inherit name;
+    shortcut = name;
+    engine = "xpath";
+    search_url = "http://${domain}/search?q={query}&pageno={pageno}&lang=en";
+    paging = true;
+    title_xpath = "//article/h3/a";
+    content_xpath = "//article/p";
+    url_xpath = "//article/a/@href";
+    enable_http = true;
+  };
 in
 {
   links.searxng.protocol = "http";
@@ -20,6 +31,22 @@ in
       engines = [
         { name = "bing"; disabled = true; }
         { name = "brave"; disabled = true; }
+        (torProxyEngine {
+          name = "rhscz";
+          domain = "ls67zxncmcqgn2vg74ckcwxgu5p3e5mf2khues3egkumhcqtbsg4kqyd.onion";
+        })
+        (torProxyEngine {
+          name = "oloke";
+          domain = "searxokthnxmo7ndis35jpts2tawcwvbovuy47qtavwo7oq4jgcm5gqd.onion";
+        })
+        (torProxyEngine {
+          name = "anoninet";
+          domain = "search.anoninetru5tflukgfaehun7q6khowgmymcff3gtk5oyesqazhmfxtyd.onion";
+        })
+        (torProxyEngine {
+          name = "serpensin";
+          domain = "searxngorpgnztkcftpz3ycdpyajd4q55y2nejw3tbszocht5zdh4lyd.onion";
+        })
       ];
       ui.theme_args.simple_style = "dark";
       outgoing = {
