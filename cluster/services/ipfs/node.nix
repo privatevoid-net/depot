79fca93ef9655497fd3e1af156cbbc06fc01dcc6
Max <max@privatevoid.net>
2026-05-10 14:53:20 +0200

    cluster/services/ipfs: use upstream package

--- cluster/services/ipfs/node.nix
+++ cluster/services/ipfs/node.nix
@@ -1,4 +1,4 @@
-{ cluster, config, depot, depot', lib, pkgs, ... }:
+{ cluster, config, depot, lib, pkgs, ... }:
 let
   inherit (depot.lib.meta) domain;
   cfg = config.services.ipfs;
@@ -21,7 +21,6 @@ in
 
   services.ipfs = {
     enable = true;
-    package = depot'.packages.ipfs;
     startWhenNeeded = false;
     autoMount = true;
     autoMigrate = false;
