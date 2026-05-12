dfd61e6aae77c0ae3c2e497f23f59a3373881676
Max <max@privatevoid.net>
2026-05-13 00:20:31 +0200

    nixpkgs: allow insecure jitsi-meet

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,3 +1,9 @@
+{ lib, ... }:
+
 {
-  lib.nixpkgs.config = { };
+  lib.nixpkgs.config = {
+    allowInsecurePredicate = package: builtins.elem (lib.getName package) [
+      "jitsi-meet"
+    ];
+  };
 }
