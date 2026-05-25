0d1df4a4457c4c9dedec8cc2256828b0d12d0d4c
Max <max@privatevoid.net>
2026-05-25 18:54:51 +0200

    cluster/services/circus: enable evaluator and queue-runner

--- cluster/services/circus/server.nix
+++ cluster/services/circus/server.nix
@@ -12,12 +12,22 @@ in
   services.fc-ci = {
     enable = true;
     package = depot'.inputs.circus.packages.fc-server;
+    evaluatorPackage = depot'.inputs.circus.packages.fc-evaluator;
+    queueRunnerPackage = depot'.inputs.circus.packages.fc-queue-runner;
     migratePackage = depot'.inputs.circus.packages.fc-migrate-cli;
     server.enable = true;
+    evaluator.enable = true;
+    queueRunner.enable = true;
     settings = {
       server = {
         host = link.ipv4;
         inherit (link) port;
+      };
+      evaluator = {
+        poll_interval = 600;
+      };
+      queue_runner = {
+        poll_interval = 30;
       };
     };
   };
