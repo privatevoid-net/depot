b40487a496046b23edccba075b7df536d640c4d5
Max <max@privatevoid.net>
2026-06-02 00:45:43 +0200

    cluster/services/circus: configure for distributed mode

--- cluster/services/circus/default.nix
+++ cluster/services/circus/default.nix
@@ -1,10 +1,20 @@
-{ config, ... }:
+{ config, depot, ... }:
 
 {
   services.circus = {
     nodes.server = [ "thousandman" ];
     nixos.server = [ ./server.nix ];
     meshLinks.server.circusServer.link.protocol = "http";
+    secrets = with config.services.circus.nodes; {
+      s3Credentials = {
+        nodes = server;
+        owner = "root";
+      };
+      cacheKey = {
+        nodes = server;
+        owner = "circus";
+      };
+    };
   };
 
   ways = let
@@ -13,6 +23,14 @@
     ci = {
       target = config.hostLinks.${host}.circusServer.url;
       domainSuffix = "manic.systems";
+    };
+  };
+
+  dns.zones = config.lib.forService "circus" {
+    "manic.systems".records = {
+      circus-agent-rpc.target = map
+        (node: depot.hours.${node}.interfaces.primary.addrPublic)
+        config.services.circus.nodes.server;
     };
   };
 }
--- cluster/services/circus/server.nix
+++ cluster/services/circus/server.nix
@@ -1,6 +1,7 @@
 { cluster, config, depot, depot', ... }:
 
 let
+  inherit (cluster.config.services.circus) secrets;
   link = cluster.config.hostLinks.${config.networking.hostName}.circusServer;
 in
 
@@ -8,6 +9,14 @@ in
   imports = [
     depot.inputs.circus.nixosModules.default
   ];
+
+  security.acme.certs."circus-agent-rpc.manic.systems" = {
+    dnsProvider = "exec";
+    group = "circus";
+    reloadServices = [
+      "circus-queue-runner.service"
+    ];
+  };
 
   services.circus = {
     enable = true;
@@ -27,9 +36,42 @@ in
       evaluator = {
         poll_interval = 600;
       };
+      cache = {
+        secret_key_file = secrets.cacheKey.path;
+        public_url = "https://ci.manic.systems/nix-cache";
+      };
       queue_runner = {
         poll_interval = 30;
+        rpc = {
+          bind = "0.0.0.0:8443";
+          auth_tokens = [
+            "ed611f88c7fb27103afba7ddad8733adf06f56e8837eba505c96349ef5fbfd8c"
+            "d74548cafebeec602bd92a0bae0822ffecefd95fcf04b51c387af19ff043d356"
+          ];
+          max_connections = 32;
+          heartbeat_ttl_secs = 60;
+          tls = {
+            cert_file = "${config.security.acme.certs."circus-agent-rpc.manic.systems".directory}/fullchain.pem";
+            key_file = "${config.security.acme.certs."circus-agent-rpc.manic.systems".directory}/key.pem";
+          };
+          cache_substituter = "https://ci.manic.systems/nix-cache";
+          cache_public_key = "ci.manic.systems-1:5EoSMEa0D55JxnmC0yr2L7/2eHr/nHH0XWupc+Z9z88=";
+        };
+      };
+      cache_upload = {
+        enabled = true;
+        store_uri = "s3://circus-cache-ndkyblidbo7zphxq38qq169tvs6ifjkg?region=fsn1&endpoint=https://fsn1.your-objectstorage.com";
+        s3 = {
+          region = "fsn1";
+          endpoint_url = "https://fsn1.your-objectstorage.com";
+        };
       };
     };
+  };
+
+  systemd.services = {
+    circus-queue-runner.serviceConfig.EnvironmentFile = [
+      secrets.s3Credentials.path
+    ];
   };
 }
