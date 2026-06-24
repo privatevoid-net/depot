099a0e54ff7201bb4e3c554065104078cdfbcb00
Max <max@privatevoid.net>
2026-06-24 15:48:23 +0200

    modules/chromium: set up enterprise policy

--- modules/chromium/default.nix
+++ modules/chromium/default.nix
@@ -1,11 +1,27 @@
 { depot', ... }:
 
+let
+  extensions = depot'.inputs.nix-crx.packages;
+in
+
 {
   programs.chromium = {
     enable = true;
+    extraOpts = {
+      EnterpriseProfileBadgeToolbarSettings = 1;
+      PasswordManagerEnabled = false;
+      ExtensionSettings = {
+        ${extensions.ublock-origin.id} = {
+          toolbar_pin = "force_pinned";
+        };
+      };
+      MandatoryExtensionsForIncognitoNavigation = with extensions; [
+        ublock-origin.id
+      ];
+    };
   };
 
-  environment.systemPackages = with depot'.inputs.nix-crx.packages; [
+  environment.systemPackages = with extensions; [
     bitwarden
     i-still-dont-care-about-cookies
     privacy-badger
